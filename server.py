3ddaf77481a6519615f256250e8bde8d3334a4fa
ksai7991 <76088923+ksai7991@users.noreply.github.com>
2024-04-30 01:25:21 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 import os
 import http.server
 import socketserver
+import aiohttp
+import asyncio
 
 from http import HTTPStatus
 
@@ -17,3 +19,20 @@ port = int(os.getenv('PORT', 80))
 print('Listening on port %s' % (port))
 httpd = socketserver.TCPServer(('', port), Handler)
 httpd.serve_forever()
+
+
+
+async def fetch_data(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            if response.status == 200:
+                data = await response.text()
+                print("Response:", data)
+            else:
+                print(f"Request failed with status code: {response.status}")
+
+async def main():
+    url = "https://orca-app-g3w23.ondigitalocean.app/sample/hello"
+    await fetch_data(url)
+
+asyncio.run(main())
